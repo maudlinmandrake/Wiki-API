9d3ed453d7169f17d9d830bf52ba368add41cc9f
Jennifer Mikac <jennymikac@gmail.com>
2020-08-04 10:40:22 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+//jshint esversion:6
+
+const express = require("express");
+const mongoose = require("mongoose");
+const ejs= require("ejs");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/wikiDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
+
+// database setup ======================================================
+
+const articlesSchema = {
+    title: String,
+    content: String,
+}
+
+const article = mongoose.model("Article", articlesSchema);
+
+
+
+
+
+
+
+// server listen ============================================================
+
+app.listen(3000, function(){
+    console.log("Server is up and running on port 3000")
+})
