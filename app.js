823544d842cc6a9f42d9ed8356cf011d21337a7d
Jennifer Mikac <jennymikac@gmail.com>
2020-08-04 14:39:15 -0400

    Make GET function for API

--- app.js
+++ app.js
@@ -26,6 +26,18 @@ const articleSchema = {
 const Article = mongoose.model("Article", articleSchema);
 
 
+app.get("/articles", function(req, res){
+    Article.find({}, function(err, foundArticles){
+        if (!err) {
+            res.send(foundArticles);
+        } else {
+            res.send(err);
+        }
+    });
+});
+
+
+
 // server listen ============================================================
 
 app.listen(3000, function(){
