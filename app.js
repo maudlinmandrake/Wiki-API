305dc58631c11cae7a480efb85095e7beafd2f23
Jennifer Mikac <jennymikac@gmail.com>
2020-08-05 09:18:36 -0400

    Refactor GET, POST and DELETE

--- app.js
+++ app.js
@@ -25,18 +25,41 @@ const articleSchema = {
 
 const Article = mongoose.model("Article", articleSchema);
 
-
-app.get("/articles", function(req, res){
-    Article.find({}, function(err, foundArticles){
-        if (!err) {
-            res.send(foundArticles);
-        } else {
-            res.send(err);
-        }
+app.route("/articles")
+    .get(
+        function(req, res){
+            Article.find({}, function(err, foundArticles){
+                if (!err) {
+                    res.send(foundArticles);
+                } else {
+                    res.send(err);
+                }
+            });
+        })
+    .post(
+        function(req, res){
+            const newArticle = new Article({
+                title: req.body.title,
+                content: req.body.content
+            });
+        
+            newArticle.save(function(err){
+                if (!err){
+                    res.send("Successfully added new article");
+                } else {
+                    res.send(err);
+                }
+            });
+        })
+    .delete(function(req, res){
+        Article.deleteMany(function(err){
+            if (!err){
+                res.send("Successfully deleted all articles");
+            } else {
+                res.send(err);
+            }
+        });
     });
-});
-
-
 
 // server listen ============================================================
 
